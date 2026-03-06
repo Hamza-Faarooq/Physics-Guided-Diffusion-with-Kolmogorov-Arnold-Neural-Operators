a762b6fb046f5b4b08e3d08673f209fd529f3acc
Muhammad Hamza <156505688+Hamza-Faarooq@users.noreply.github.com>
2026-03-06 19:01:11 +0530

    Create spectral_conv.py

---
+++ models/spectral_conv.py
@@ -0,0 +1,55 @@
+import torch
+import torch.nn as nn
+import torch.fft
+
+
+class SpectralConv2d(nn.Module):
+
+    def __init__(self, in_channels, out_channels, modes1, modes2):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes1 = modes1
+        self.modes2 = modes2
+
+        self.scale = 1 / (in_channels * out_channels)
+
+        self.weights = nn.Parameter(
+            self.scale * torch.rand(
+                in_channels,
+                out_channels,
+                modes1,
+                modes2,
+                dtype=torch.cfloat
+            )
+        )
+
+    def compl_mul2d(self, input, weights):
+
+        return torch.einsum("bixy,ioxy->boxy", input, weights)
+
+    def forward(self, x):
+
+        batchsize = x.shape[0]
+
+        x_ft = torch.fft.rfft2(x)
+
+        out_ft = torch.zeros(
+            batchsize,
+            self.out_channels,
+            x.size(-2),
+            x.size(-1) // 2 + 1,
+            dtype=torch.cfloat,
+            device=x.device
+        )
+
+        out_ft[:, :, :self.modes1, :self.modes2] = \
+            self.compl_mul2d(
+                x_ft[:, :, :self.modes1, :self.modes2],
+                self.weights
+            )
+
+        x = torch.fft.irfft2(out_ft, s=(x.size(-2), x.size(-1)))
+
+        return x
